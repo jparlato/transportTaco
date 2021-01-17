ffde24cbbba6faf6d823f86e9526abe73815c5a8
jparlato@mindspring.com <jparlato@mindspring.com>
2021-01-17 11:00:03 -0500

    4 PASSING 1 FAILING TESTS

--- src/app/services/taco-service.spec.ts
+++ src/app/services/taco-service.spec.ts
@@ -18,22 +18,22 @@ fdescribe('Taco Service', () => {
   let httpMock: HttpTestingController;
 
   // tslint:disable-next-line: one-variable-per-declaration
-  let tacoService: TacoService,
-    mockService = {
-      findTacoToppingTypes: jasmine
-        .createSpy('findTacoToppingTypes')
-        .and.returnValue(of(toppingMock)),
-    };
+  // let tacoService: TacoService,
+  //   mockService = {
+  //     findTacoToppingTypes: jasmine
+  //       .createSpy('findTacoToppingTypes')
+  //       .and.returnValue(of(toppingMock)),
+  //   };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [{ provide: TacoService, useValue: mockService }],
+      providers: [{ provide: TacoService }],
     });
 
     spyOn(HttpClient.prototype, 'request').and.callThrough();
 
-    service = TestBed.inject(TacoService);
+    // service = TestBed.inject(TacoService);
     httpMock = TestBed.inject(HttpTestingController);
     httpClient = TestBed.inject(HttpClient);
   });
@@ -46,9 +46,11 @@ fdescribe('Taco Service', () => {
     expect(service).toBeTruthy();
   });
 
-  it('findTacoToppingTypes() should call http Get method for the given route', () => {
+  it('findTacoToppingTypes() should NOT call http Get method for the given route', () => {
     // Arrange
     // Set Up Data
+
+    spyOn(service, `findTacoToppingTypes`).and.returnValue(of(toppingMock));
 
     // Act
     service.findTacoToppingTypes().subscribe((topping) => {
@@ -60,11 +62,33 @@ fdescribe('Taco Service', () => {
 
     const req = httpMock.expectNone({
       method: 'GET',
-      url: `findTacoToppingTypes`,
+      url: `http://localhost:4200/api/findTacoToppingTypes`,
     });
 
-    // Assert -3
-    // req.flush(toppingMock);
+    httpMock.verify();
+  });
+
+  it('findTacoToppingTypes() should call http Get method for the given route', () => {
+    // Arrange
+    // Set Up Data
+
+    spyOn(service, `findTacoToppingTypes`).and.returnValue(of(toppingMock));
+
+    // Act
+    service.findTacoToppingTypes().subscribe((topping) => {
+      // Assert-1
+      expect(topping).toEqual(toppingMock);
+    });
+
+    // Assert -2
+
+    const req = httpMock.expectOne({
+      method: 'GET',
+      url: `http://localhost:4200/api/findTacoToppingTypes`,
+    });
+
+    // Assert - 3;
+    req.flush(toppingMock);
 
     httpMock.verify();
   });
@@ -73,6 +97,7 @@ fdescribe('Taco Service', () => {
     // Arrange
     // Set Up Data
 
+    spyOn(service, 'findTacoToppingTypes').and.returnValue(of(toppingMock));
     // Act
     service.findTacoToppingTypes().subscribe((topping) => {
       // Assert-1
@@ -82,6 +107,23 @@ fdescribe('Taco Service', () => {
     httpMock.verify();
   });
 
+  it('findRecipeById() should call service and find specific recipe', () => {
+    // Arrange
+    const RecipeOne: Recipe = JSON.parse(
+      '{"recipeName":"SamsOne","shellType":"GLUTENFREE","toppings":["CHEESE","SOUR CREAM"],"proteinType":"MEAT","id":"1"}'
+    );
+
+    spyOn(service, 'findRecipeById').and.returnValue(of(RecipeOne));
+    // Set Up Data
+
+    // Act
+    service.findRecipeById(RecipeOne.id ?? 1).subscribe((recipe) => {
+      // Assert-1
+      expect(recipe).toEqual(RecipeOne);
+    });
+
+    httpMock.verify();
+  });
   it('findRecipeById() should call service and find specific recipe', () => {
     // Arrange
     const RecipeOne: Recipe = JSON.parse(
