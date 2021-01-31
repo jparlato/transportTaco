ea82816a6c346b52a1c18b99a0c781f98a76f19b
jparlato@mindspring.com <jparlato@mindspring.com>
2021-01-31 12:26:20 -0500

    test

--- src/app/shared/ui.actions.ts
+++ src/app/shared/ui.actions.ts
@@ -6,7 +6,7 @@ export const STOP_LOADING = '[UI] Stop Loading';
 export class StartLoading implements Action {
   readonly type = START_LOADING;
 }
-
+// test
 export class StopLoading implements Action {
   readonly type = STOP_LOADING;
 }
